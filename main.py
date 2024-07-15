54bcc377858b14b390274dede2ef12858ef7862a
Hapurobokka <flaco.legos@gmail.com>
2024-07-14 19:15:48 -0600

    Pavimentando el camino para cambiar toda la db

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ DATABASE = "database.db"
 def run_query(query, parameters=()) -> sqlite3.Cursor:
     "Runs a query for the external database"
     with sqlite3.connect(DATABASE) as conn:
-        cursor: sqlite3.Cursor = conn.cursor()
-        result: sqlite3.Cursor = cursor.execute(query, parameters)
+        cursor = conn.cursor()
+        result = cursor.execute(query, parameters)
         conn.commit()
     return result
 
@@ -36,7 +36,41 @@ def check_if_in_database(table_to_check, column_to_check, data):
 
 
 def search_id(table, row, data):
-    return request_data(f'SELECT id FROM {table} WHERE {row} = ?', (data, ))[0][0]
+    "Returns the id for the given data"
+    try:
+        return request_data(f'SELECT id FROM {table} WHERE {row} = ?', (data, ))[0][0]
+    except IndexError:
+        return None
+
+# ------------------------------------------------------------------------------
+
+def check_registers(current_register):
+    searched_register = request_data('SELECT id FROM registers WHERE date_id = ? AND shift_id = ? AND employee_id = ?', current_register)
+
+    if not searched_register:
+        print("Este turno parece no estar registrado en la base de datos, ¿quiere registrarlo?")
+
+        if input("> ") != "y":
+            return False
+
+        add_data('INSERT INTO registers VALUES (NULL, ?, ?, ?)', current_register)
+        return False
+
+    return searched_register[0][0]
+
+
+def get_current_register_info(register_id):
+    query = """
+    SELECT d.date, s.shift, e.employee_name
+    FROM registers r
+    JOIN dates d ON d.id = r.date_id
+    JOIN shifts s ON s.id = r.shift_id
+    JOIN employees e on e.id = r.employee_id
+    WHERE r.id = ?"""
+    try:
+        return request_data(query, (register_id, ))[0]
+    except IndexError:
+        return (None, None, None)
 
 
 def get_total_machine_prices(acc, db_rows):
@@ -47,7 +81,7 @@ def get_total_machine_prices(acc, db_rows):
         return acc
 
 
-def ask_for_information(information, table, row):
+def ask_for_information(table, row, information):
     """
     Asks for input on the given data. If it already exists in the table, returns it's id.
     Otherwise, asks if the user wants to add it to the table"""
@@ -66,20 +100,33 @@ def ask_for_information(information, table, row):
 
 # ------------------------------------------------------------------------------
 
-def main(date_id, shift_id, employee_id):
-    print("Por favor introduza la siguiente información.")
-    if date_id == None:
-        return main(ask_for_information('fecha', 'dates', 'date'), shift_id, employee_id)
+def show_current_register_info(register_id):
+    current_register_info = get_current_register_info(register_id)
+    print(f"Hoy es {current_register_info[0].capitalize()}.") # type: ignore
+    print(f"En el turno de la {current_register_info[1].capitalize()}.") # type: ignore
+    print(f"Y esta trabajando {current_register_info[2].capitalize()}.") # type: ignore
 
-    if shift_id == None:
-        print("Por favor introduzca un turno: ")
-        current_shift_id = int(input("TURNO: "))
-        return main(date_id, current_shift_id, employee_id)
+def main(date_id, shift_id, employee_id, register_id):
+    if not register_id:
+        if date_id is None:
+            return main(ask_for_information('dates', 'date', 'fecha'), shift_id, employee_id, register_id)
 
-    if employee_id == None:
-        return main(date_id, shift_id, ask_for_information('empleado', 'employees', 'employee_name'))
+        if shift_id is None:
+            current_shift_id = int(input("TURNO: "))
+            return main(date_id, current_shift_id, employee_id, register_id)
 
-    print(date_id, shift_id, employee_id)
+        if employee_id is None:
+            return main(date_id, shift_id, ask_for_information('employees', 'employee_name', 'empleado'), register_id)
+
+        current_register_id = check_registers((date_id, shift_id, employee_id))
+
+        if not current_register_id:
+            return main(date_id, shift_id, employee_id, register_id)
+
+        return main(date_id, shift_id, employee_id, current_register_id)
+
+    show_current_register_info(register_id)
 
 
-if __name__ == "__main__": main(None, None, None)
+if __name__ == "__main__":
+    main(None, None, None, None)
