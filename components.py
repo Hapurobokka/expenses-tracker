a86ea0a3f4629d17590682bae7c4275fe6f95ec1
Hapurobokka <flaco.legos@gmail.com>
2024-07-30 19:48:02 -0600

    Operaciones básicas añadidas. Estoy muerto.

--- components.py
+++ components.py
@@ -36,8 +36,8 @@ def setup_products_tree(tree):
     core.fill_table(tree, query, REGISTER_ID)
 
 
-def setup_simple_tree(tree, table, values, pos):
-    tree.grid(row=pos[0], column=pos[1], sticky="nsew", columnspan=3) 
+def setup_simple_tree(tree, table, values):
+    tree.grid(row=1, column=0, sticky="nsew", columnspan=3) 
 
     tree.heading("#0", text="ID", anchor=tkinter.CENTER)
     tree.heading("name", text="Nombre", anchor=tkinter.CENTER)
@@ -50,34 +50,70 @@ def setup_simple_tree(tree, table, values, pos):
     query = f'SELECT {core.comma_separated_string(values)} FROM {table} WHERE register_id = ?'
 
     core.fill_table(tree, query, REGISTER_ID)
-    
+
+
+def create_tree_container(root, frame_pos: tuple[int, int], frame_text: str, table: str, table_values: list[str]):
+    tree_container = {}
+
+    tree_container["table"] = table
+    tree_container["table_values"] = table_values
+
+    tree_container["frame"] = tkinter.Frame(root)
+    tree_container["frame"].grid(
+        row=frame_pos[0], column=frame_pos[1], padx=10, pady=10, sticky="nsew"
+    )
+
+    tree_container["label"] = tkinter.Label(tree_container["frame"], text=frame_text)
+    tree_container["label"].grid(row=0, column=0, columnspan=3)
+
+    tree_container["tree"] = ttk.Treeview(tree_container["frame"], columns=["name", "amount"])
+    tree_container["tree"].grid(row=0, column=1, columnspan=3)
+
+    tree_container["buttons"] = []
+
+    tree_container["buttons"].append(tkinter.Button(tree_container["frame"], text="Añadir"))
+    tree_container["buttons"].append(tkinter.Button(tree_container["frame"], text="Eliminar"))
+    tree_container["buttons"].append(tkinter.Button(tree_container["frame"], text="Editar"))
+
+    tree_container["buttons"][0].grid(row=2, column=0)
+    tree_container["buttons"][1].grid(row=2, column=1)
+    tree_container["buttons"][2].grid(row=2, column=2)
+
+    tree_container["buttons"][0].bind("<Button-1>", lambda _: ev.add_record(root, tree_container, REGISTER_ID))
+    tree_container["buttons"][1].bind("<Button-1>", lambda _: ev.erase_record(tree_container, REGISTER_ID))
+    tree_container["buttons"][2].bind("<Button-1>", lambda _: ev.alter_record(tree_container, root, REGISTER_ID))
+
+    setup_simple_tree(tree_container["tree"], table, table_values)
+
+    return tree_container
 
 def entry_point(root):
     root.title("Expense Tracker")
 
+    machine_container = create_tree_container(root,
+                                              (0, 0),
+                                              "Premios de maquinas",
+                                              'machine_table',
+                                              ['id', 'machine_name', 'amount'])
+    replenish_container = create_tree_container(root,
+                                                (0, 1),
+                                                "Reposiciones de maquinas",
+                                                'replenishments',
+                                                ['id', 'machine_name', 'amount'])
+    expenses_container = create_tree_container(root,
+                                               (1, 1),
+                                               "Gastos",
+                                               'expenses',
+                                               ['id', 'concept', 'amount'])
+
     # Creando frames
-    machine_frame = tkinter.Frame(root)
-    machine_frame.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
-
-    replenish_frame = tkinter.Frame(root)
-    replenish_frame.grid(row=0, column=1, padx=10, pady=10, sticky="nsew")
-
-    expenses_frame = tkinter.Frame(root)
-    expenses_frame.grid(row=1, column=1, padx=10, pady=10, sticky="nsew")
-
     products_sales_frame = tkinter.Frame(root)
     products_sales_frame.grid(row=0, column=2, padx=10, pady=10, sticky="nsew")
 
     # Creando labels 
-    tkinter.Label(machine_frame, text="Premios de maquinas").grid(row=0, column=0, columnspan=3)
-    tkinter.Label(replenish_frame, text="Reposiciones de maquinas").grid(row=0, column=0)
-    tkinter.Label(expenses_frame, text="Gastos").grid(row=0, column=0)
     tkinter.Label(products_sales_frame, text="Ventas productos").grid(row=0, column=0)
 
     # Crea los treeviews
-    machine_tree = ttk.Treeview(machine_frame, columns=["name", "amount"])
-    replenish_tree = ttk.Treeview(replenish_frame, columns=["name", "amount"])
-    expenses_tree = ttk.Treeview(expenses_frame, columns=["name", "amount"])
     products_sales_tree = ttk.Treeview(products_sales_frame, columns=["product_name", "in_product", "out_product", "profits"])
 
     # Acomodamos nuestro único botón lol
@@ -85,24 +121,6 @@ def entry_point(root):
     btn_products.grid(row=2, column=0)
     btn_products.bind("<Button-1>", lambda _: ev.show_products(root))
 
-    btn_machine_add = tkinter.Button(machine_frame, text="Añadir")
-    btn_machine_add.grid(row=2, column=0)
-    btn_machine_add.bind("<Button-1>", lambda _: ev.add_record(root,
-                                                               machine_tree,
-                                                               'machine_table',
-                                                               ['register_id', 'machine_name', 'amount'],
-                                                                REGISTER_ID))
-
-    btn_machine_delete = tkinter.Button(machine_frame, text="Borrar")
-    btn_machine_delete.grid(row=2, column=1)
-
-    btn_machine_edit = tkinter.Button(machine_frame, text="Editar")
-    btn_machine_edit.grid(row=2, column=2)
-
-    # Los llena con datos de la base de datos
-    setup_simple_tree(machine_tree, 'machine_table', ['id', 'machine_name', 'amount'], (1, 0))
-    setup_simple_tree(replenish_tree, 'replenishments', ['id', 'machine_name', 'amount'], (1, 0))
-    setup_simple_tree(expenses_tree, 'expenses', ['id', 'concept', 'amount'], (1, 0))
     # Función propia porque no mames
     setup_products_tree(products_sales_tree)
 
--- events.py
+++ events.py
@@ -1,3 +1,4 @@
+from os import pread
 import tkinter
 from tkinter import ttk
 import core
@@ -32,7 +33,7 @@ def show_products(window):
 
     core.fill_table(products_tree, query)
 
-def perform_add_record(table, tree, values, register_id, en_name, en_amount):
+def perform_add_record(tree_container, register_id, en_name, en_amount):
     if not validate_fields([en_name.get(), en_amount.get()]):
         return
 
@@ -43,14 +44,17 @@ def perform_add_record(table, tree, values, register_id, en_name, en_amount):
         return
 
     query = f"""
-    SELECT id, {core.comma_separated_string(values[1:])} FROM {table} WHERE register_id = ?
+    SELECT id, {core.comma_separated_string(tree_container["table_values"][1:])} 
+    FROM {tree_container["table"]} WHERE register_id = ?
     """
-    core.create_record(table, values, (register_id, record_name, record_amount))
-    core.fill_table(tree, query, register_id)
+    core.create_record(tree_container["table"],
+                       ['register_id', *tree_container["table_values"][1:]],
+                       (register_id, record_name, record_amount))
+    core.fill_table(tree_container["tree"], query, register_id)
     en_name.delete(0, tkinter.END)
     en_amount.delete(0, tkinter.END)
 
-def add_record(root, tree, table, values, register_id):
+def add_record(root, tree_container, register_id):
     add_window = tkinter.Toplevel(root)
     add_window.title("Crear nuevo registro")
 
@@ -67,6 +71,77 @@ def add_record(root, tree, table, values, register_id):
 
     add_button = tkinter.Button(add_window, text="Añadir")
     add_button.bind("<Button-1>", lambda _: perform_add_record(
-        table, tree, values, register_id, en_name, en_amount)
+        tree_container, register_id, en_name, en_amount)
     )
     add_button.pack()
+
+def check_valid_selection(tree: ttk.Treeview):
+    try:
+        selected_item = tree.selection() 
+        _ = tree.item(selected_item)['values'][0] # type: ignore
+    except IndexError:
+        return False
+
+    return True
+
+def erase_record(tree_container, register_id):
+    if not check_valid_selection(tree_container["tree"]):
+        return
+    id = tree_container["tree"].item(tree_container["tree"].selection())['text']
+    core.delete_record(tree_container["table"], id)
+    query = f"""
+    SELECT id, {core.comma_separated_string(tree_container["table_values"][1:])} 
+    FROM {tree_container["table"]} WHERE register_id = ?
+    """
+    core.fill_table(tree_container["tree"], query, register_id)
+
+def perform_alter_record(edit_wind, tree_container, id, register_id, en_new_name, en_new_amount):
+    if not validate_fields([en_new_name.get(), en_new_amount.get()]):
+        return
+
+    new_name = en_new_name.get().upper()
+    try:
+        new_amount = int(en_new_amount.get())
+    except ValueError:
+        return
+
+    query = f"""UPDATE {tree_container["table"]}
+    SET {tree_container["table_values"][1]} = ?, {tree_container["table_values"][2]} = ? 
+    WHERE id = ?"""
+
+    core.run_query(query, (new_name, new_amount, id))
+    edit_wind.destroy()
+
+    query2 = f"""
+    SELECT id, {core.comma_separated_string(tree_container["table_values"][1:])} 
+    FROM {tree_container["table"]} WHERE register_id = ?
+    """
+    core.fill_table(tree_container["tree"], query2, register_id)
+
+def alter_record(tree_container, root, register_id):
+    if not check_valid_selection(tree_container["tree"]):
+        return
+    id = tree_container["tree"].item(tree_container["tree"].selection())['text']
+    old_name = tree_container["tree"].item(tree_container["tree"].selection())['values'][0]
+    old_amount = tree_container["tree"].item(tree_container["tree"].selection())['values'][1]
+
+    edit_wind = tkinter.Toplevel(root)
+    edit_wind.title("Editar registro")
+
+    tkinter.Label(edit_wind, text="Nombre anterior: ").grid(row=0, column=1)
+    tkinter.Entry(edit_wind, textvariable=tkinter.StringVar(edit_wind, value=old_name), state="readonly").grid(row=0, column=2)
+
+    tkinter.Label(edit_wind, text="Nuevo nombre: ").grid(row=1, column=1)
+    new_name = tkinter.Entry(edit_wind)
+    new_name.grid(row=1, column=2)
+
+    tkinter.Label(edit_wind, text="Old Price: ").grid(row=2, column=1)
+    tkinter.Entry(edit_wind, textvariable=tkinter.StringVar(edit_wind, value=old_amount), state="readonly").grid(row=2, column=2)
+
+    tkinter.Label(edit_wind, text="New Price: ").grid(row=3, column=1)
+    new_amount = tkinter.Entry(edit_wind)
+    new_amount.grid(row=3, column=2)
+
+    btn_edit = tkinter.Button(edit_wind, text="Editar")
+    btn_edit.bind("<Button-1>", lambda _ : perform_alter_record(edit_wind, tree_container, id, register_id, new_name, new_amount))
+    btn_edit.grid(row=4, column=2, sticky="we")
