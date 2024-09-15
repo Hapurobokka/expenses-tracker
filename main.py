acb0e8251b3062a0dae0b1cfbb29d26cad04397a
Hapurobokka <flaco.legos@gmail.com>
2024-09-15 01:19:48 -0600

    ahora mostramos los datos del registro actual

--- main.py
+++ main.py
@@ -7,14 +7,64 @@ Por Hapurobokka.
 import tkinter as tk
 from containers import TreeContainer, ProductsContainer, TotalsContainer
 import events as ev
+import core
 
 
-REGISTER_ID = 7
+REGISTER_ID = 8
+
+def get_register_info(root, register_id):
+    data_query = """
+    SELECT e.employee_name, s.shift_name, d.date
+    FROM registers r
+    JOIN employees e ON e.id = r.employee_id
+    JOIN shifts s ON s.id = r.shift_id
+    JOIN dates d ON d.id = r.date_id
+    WHERE r.id = ?
+    """
+    data = core.request_data(data_query, (register_id,))[0]
+    register_info = {}
+
+    register_info["employee"] = tk.StringVar(root, value=data[0])
+    register_info["shift"] = tk.StringVar(root, value=data[1])
+    register_info["date"] = tk.StringVar(root, value=data[2])
+
+    return register_info
 
 
-def entry_point(root, register_id):
+def create_register_display(root, register_info):
+    frame = tk.Frame(root)
+
+    employee_frame = tk.LabelFrame(frame, text="Empleado")
+    employee_frame.grid(row=0, column=0)
+    tk.Label(employee_frame, textvariable=register_info["employee"]).grid(row=0, column=0)
+
+    shift_frame = tk.LabelFrame(frame, text="Turno")
+    shift_frame.grid(row=0, column=1)
+    tk.Label(shift_frame, textvariable=register_info["shift"]).grid(row=0, column=0)
+
+    date_frame = tk.LabelFrame(frame, text="Fecha")
+    date_frame.grid(row=0, column=2)
+    tk.Label(date_frame, textvariable=register_info["date"]).grid(row=0, column=0)
+
+    return frame
+
+
+def get_lastest_register():
+    query = """
+    SELECT * FROM registers
+    ORDER BY id DESC
+    LIMIT 1
+    """
+
+    return core.request_data(query)[0][0]
+
+def entry_point(root):
     """Punto de entrada para el programa"""
     root.title("Expense Tracker")
+
+    register_id = get_lastest_register()
+
+    register_info = get_register_info(root, register_id)
 
     total_expenses_frame = tk.Frame(root)
 
@@ -65,6 +115,10 @@ def entry_point(root, register_id):
         command=lambda: ev.spawn_add_register_window(containers, root),
     ).grid(row=0, column=0)
 
+    register_display_frame = create_register_display(root, register_info)
+
+    register_display_frame.grid(row=0, column=1)
+
     containers["machine_container"].frame.grid(
         row=1, column=0, padx=10, pady=10, sticky="nsew"
     )
@@ -83,5 +137,5 @@ def entry_point(root, register_id):
 
 
 window = tk.Tk()
-entry_point(window, REGISTER_ID)
+entry_point(window)
 window.mainloop()
